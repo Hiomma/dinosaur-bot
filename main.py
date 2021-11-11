40a4426d8ca21c35394ac702d3b0cd102b24377c
Gabriel Sereno <gabrielf2@live.com>
2021-11-11 19:15:50 -0300

    Initial Commit

---
+++ main.py
@@ -0,0 +1,23 @@
+import python
+import os
+import random
+import sys
+
+SCREEN_HEIGHT = 600
+SCREEN_WIDTH = 1100
+SCREEN = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+RUNNING = [
+    pygame.image.load(os.path.join("assets/dino", "dino-running-1.png")),
+    pygame.image.load(os.path.join("assets/dino", "dino-running-2.png"))
+           ]
+
+JUMPING =
+
+BACKGROUND =
+
+FONT =
+
+pygame.init()
+
+def main():
